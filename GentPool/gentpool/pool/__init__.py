f691e2789ce2c8cd0c7b4c1f5f6657e68fc7f5bf
Sagar21999 <51937895+Sagar21999@users.noreply.github.com>
2024-03-08 17:59:27 -0500

    initial commit for vanilla agent and scholar agent

--- GentPool/gentpool/pool/__init__.py
+++ GentPool/gentpool/pool/__init__.py
@@ -9,3 +9,5 @@ from .vanilla_template import *
 from .openai_memory_template import *
 from .vanilla-ziyu import *
 from .scholar-ziyu import *
+from .my_first_agent import *
+from .my_scholar_agent import *
